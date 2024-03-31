c59edf616ffe09635f3a62805202f519bd64524a
maceip <ryan.macarthur@gmail.com>
2024-03-30 20:04:49 -0400

    initial dkg guessing service

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { v4 as uuidv4 } from 'uuid';
-import { Server, StableBTreeMap, ic } from 'azle';
-import express from 'express';
+import { v4 as uuidv4 } from "uuid";
+import { Server, StableBTreeMap, ic } from "azle";
+import express from "express";
+import * as secp from "@noble/secp256k1";
 
 /**
  * `messagesStorage` - it's a key-value datastructure that is used to store messages.
@@ -8,11 +9,11 @@ import express from 'express';
  * For the sake of this contract we've chosen {@link StableBTreeMap} as a storage for the next reasons:
  * - `insert`, `get` and `remove` operations have a constant time complexity - O(1)
  * - data stored in the map survives canister upgrades unlike using HashMap where data is stored in the heap and it's lost after the canister is upgraded
- * 
+ *
  * Brakedown of the `StableBTreeMap(string, Message)` datastructure:
  * - the key of map is a `messageId`
  * - the value in this map is a message itself `Message` that is related to a given key (`messageId`)
- * 
+ *
  * Constructor values:
  * 1) 0 - memory id where to initialize a map.
  */
@@ -20,68 +21,82 @@ import express from 'express';
 /**
     This type represents a message that can be listed on a board.
 */
-class Message {
-    id: string;
-    title: string;
-    body: string;
-    attachmentURL: string;
-    createdAt: Date;
-    updatedAt: Date | null
+
+class PrivateKey {
+  createdAt: Date;
+  id: string;
+  gameId: string;
+  sk: Uint8Array[];
+  pk: Uint8Array[];
+}
+interface Guess {
+  createdAt: Date;
+  id: string;
+  gameId: string;
+  user: string;
+  guess: string;
+}
+interface LiarGame {
+  createdAt: Date;
+  id: string;
+  isDemo: boolean | null | undefined;
+  gameId: string;
+  key: PrivateKey;
+  guesses: Guess[];
+  won: boolean;
+}
+interface Db {
+  games: LiarGame[];
 }
 
-const messagesStorage = StableBTreeMap<string, Message>(0);
+const allGames = StableBTreeMap<string, Db>(0);
+const guessStorage = StableBTreeMap<string, Guess>(0);
+const keyStorage = StableBTreeMap<string, PrivateKey>(0);
+const gameStorage = StableBTreeMap<string, LiarGame>(0);
 
 export default Server(() => {
-    const app = express();
-    app.use(express.json());
-    
-    app.post("/messages", (req, res) => {
-        const message: Message =  {id: uuidv4(), createdAt: getCurrentDate(), ...req.body};
-        messagesStorage.insert(message.id, message);
-        res.json(message);
-    });
+  const app = express();
+  app.use(express.json());
 
-    app.get("/messages", (req, res) => {
-        res.json(messagesStorage.values());
-    });
+  app.post("/guess", (req, res) => {
+    const guess: Guess = {
+      id: uuidv4(),
+      createdAt: getCurrentDate(),
+      ...req.body,
+    };
+    guessStorage.insert(guess.id, guess);
+    res.json(guess);
+  });
 
-    app.get("/messages/:id", (req, res) => {
-        const messageId = req.params.id;
-        const messageOpt = messagesStorage.get(messageId);
-        if ("None" in messageOpt) {
-            res.status(404).send(`the message with id=${messageId} not found`);
-        } else {
-            res.json(messageOpt.Some);
-        }
-    });
+  app.post("/game", (req, res) => {
+    const privKey = secp.utils.randomPrivateKey(); // Secure random private key
+    const pubKey = secp.getPublicKey(privKey);
 
-    app.put("/messages/:id", (req, res) => {
-        const messageId = req.params.id;
-        const messageOpt = messagesStorage.get(messageId);
-        if ("None" in messageOpt) {
-            res.status(400).send(`couldn't update a message with id=${messageId}. message not found`);
-        } else {
-            const message = messageOpt.Some;
-            const updatedMessage = { ...message, ...req.body, updatedAt: getCurrentDate()};
-            messagesStorage.insert(message.id, updatedMessage);
-            res.json(updatedMessage);
-        }
-    });
+    const key: PrivateKey = {
+      id: uuidv4(),
+      sk: privKey,
+      pk: pubKey,
+      createdAt: getCurrentDate(),
+      ...req.body,
+    };
+    const game: LiarGame = {
+      id: uuidv4(),
+      key: key,
+      createdAt: getCurrentDate(),
+      ...req.body,
+    };
+    gameStorage.insert(game.id, game);
+    res.json(game);
+  });
 
-    app.delete("/messages/:id", (req, res) => {
-        const messageId = req.params.id;
-        const deletedMessage = messagesStorage.remove(messageId);
-        if ("None" in deletedMessage) {
-            res.status(400).send(`couldn't delete a message with id=${messageId}. message not found`);
-        } else {
-            res.json(deletedMessage.Some);
-        }
-    });
+  app.get("/games", (req, res) => {
+    res.json(gameStorage.values());
+  });
 
-    return app.listen();
+  return app.listen();
 });
 
 function getCurrentDate() {
-    const timestamp = new Number(ic.time());
-    return new Date(timestamp.valueOf() / 1000_000);
+  const timestamp = new Number(ic.time());
+  return new Date(timestamp.valueOf() / 1000_000);
 }
